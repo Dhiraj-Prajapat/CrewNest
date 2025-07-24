5e461d1381a2e762a365e9a2db39e22d147837c0
Dhiraj-Prajapat <dheerajprajapat321@gmail.com>
2025-07-24 18:18:17 +0530

    thread and drafts hide

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -1,22 +1,20 @@
- 
+"use client";
 
-'use client';
+import { AlertTriangle, HashIcon, Loader } from "lucide-react";
 
-import { AlertTriangle, HashIcon, Loader, MessageSquareText, SendHorizonal } from 'lucide-react';
+import { useGetChannels } from "@/features/channels/api/use-get-channels";
+import { useCreateChannelModel } from "@/features/channels/Store/use-create-channel-model";
+import { useCurrentMember } from "@/features/members/api/use-current-member";
+import { useGetMembers } from "@/features/members/api/use-get-members";
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+import { useChannelId } from "@/hooks/use-channel-id";
+import { useMemberId } from "@/hooks/use-member-id";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
 
-import { useGetChannels } from '@/features/channels/api/use-get-channels';
-import { useCreateChannelModel } from '@/features/channels/Store/use-create-channel-model';
-import { useCurrentMember } from '@/features/members/api/use-current-member';
-import { useGetMembers } from '@/features/members/api/use-get-members';
-import { useGetWorkspace } from '@/features/workspaces/api/use-get-workspace';
-import { useChannelId } from '@/hooks/use-channel-id';
-import { useMemberId } from '@/hooks/use-member-id';
-import { useWorkspaceId } from '@/hooks/use-workspace-id';
-
-import { SidebarItem } from './sidebar-item';
-import { UserItem } from './user-item';
-import { WorkspaceHeader } from './workspace-header';
-import { WorkspaceSection } from './workspace-section';
+import { SidebarItem } from "./sidebar-item";
+import { UserItem } from "./user-item";
+import { WorkspaceHeader } from "./workspace-header";
+import { WorkspaceSection } from "./workspace-section";
 
 export const WorkspaceSidebar = () => {
   const workspaceId = useWorkspaceId();
@@ -25,10 +23,18 @@ export const WorkspaceSidebar = () => {
 
   const [_open, setOpen] = useCreateChannelModel();
 
-  const { data: member, isLoading: memberLoading } = useCurrentMember({ workspaceId });
-  const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({ id: workspaceId });
-  const { data: channels, isLoading: channelsLoading } = useGetChannels({ workspaceId });
-  const { data: members, isLoading: membersLoading } = useGetMembers({ workspaceId });
+  const { data: member, isLoading: memberLoading } = useCurrentMember({
+    workspaceId,
+  });
+  const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({
+    id: workspaceId,
+  });
+  const { data: channels, isLoading: channelsLoading } = useGetChannels({
+    workspaceId,
+  });
+  const { data: members, isLoading: membersLoading } = useGetMembers({
+    workspaceId,
+  });
 
   if (memberLoading || workspaceLoading || channelsLoading || membersLoading) {
     return (
@@ -49,19 +55,26 @@ export const WorkspaceSidebar = () => {
 
   return (
     <div className="flex h-full flex-col gap-y-2 bg-[#5E2C5F]">
-      <WorkspaceHeader workspace={workspace} isAdmin={member.role === 'admin'} />
+      <WorkspaceHeader
+        workspace={workspace}
+        isAdmin={member.role === "admin"}
+      />
 
-      <div className="mt-3 flex flex-col px-2">
+      {/* <div className="mt-3 flex flex-col px-2">
         <SidebarItem label="Threads" icon={MessageSquareText} id="threads" />
 
         <SidebarItem label="Drafts & Sent" icon={SendHorizonal} id="draft" />
-      </div>
+      </div> */}
 
       {channels && channels.length !== 0 && (
-        <WorkspaceSection label="Channels" hint="New Channel" onNew={member.role === 'admin' ? () => setOpen(true) : undefined}>
+        <WorkspaceSection
+          label="Channels"
+          hint="New Channel"
+          onNew={member.role === "admin" ? () => setOpen(true) : undefined}
+        >
           {channels?.map((item) => (
             <SidebarItem
-              variant={channelId === item._id ? 'active' : 'default'}
+              variant={channelId === item._id ? "active" : "default"}
               key={item._id}
               id={item._id}
               icon={HashIcon}
@@ -72,14 +85,18 @@ export const WorkspaceSidebar = () => {
       )}
 
       {members && members.length !== 0 && (
-        <WorkspaceSection label="Direct Messages" hint="New Direct Message" onNew={member.role === 'admin' ? () => {} : undefined}>
+        <WorkspaceSection
+          label="Direct Messages"
+          hint="New Direct Message"
+          onNew={member.role === "admin" ? () => {} : undefined}
+        >
           {members?.map((item) => (
             <UserItem
               key={item._id}
               id={item._id}
               label={item.user.name}
               image={item.user.image}
-              variant={item._id === memberId ? 'active' : 'default'}
+              variant={item._id === memberId ? "active" : "default"}
             />
           ))}
         </WorkspaceSection>
