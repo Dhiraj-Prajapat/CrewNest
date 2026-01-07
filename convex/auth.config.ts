7992d6e35d540f9f9d501532eb8d4db501face37
Dhiraj-Prajapat <dheerajprajapat321@gmail.com>
2026-01-08 00:27:14 +0530

    feat: add Convex authentication configuration

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,8 +1,7 @@
 const authConfig = {
   providers: [
     {
-      // domain: process.env.CONVEX_SITE_URL,
-      domain: process.env.AUTH_SITE_URL,
+      domain: process.env.CONVEX_SITE_URL || process.env.AUTH_SITE_URL,
       applicationID: "convex",
     },
   ],
